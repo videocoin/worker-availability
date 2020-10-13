74c6414b74c2b261542558a2cf4d80317b3bdddd
dmitry <yashulyak@gmail.com>
2020-10-13 13:58:56 +0300

    Add json tags

--- stats/collector_test.go
+++ stats/collector_test.go
@@ -2,6 +2,7 @@ package stats
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -12,11 +13,13 @@ func TestCollector(t *testing.T) {
 	appctx, err := NewContext(context.TODO(), FromEnv())
 	require.NoError(t, err)
 	Collect(appctx)
+	Collect(appctx)
 
 	records, err := appctx.DB.All(context.TODO())
 	require.NoError(t, err)
 	require.True(t, len(records) > 0)
 	for _, record := range records {
 		require.Equal(t, emitterv1.WorkerStateBonded, record.Miner.WorkerState)
+		fmt.Println(record.Miner.Name, record.Timestamp)
 	}
 }
